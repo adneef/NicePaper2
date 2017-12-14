ad2e5eea864e214441030e93dc22b54f11f297a2
Adam Neef <adneef@gmail.com>
2017-12-13 22:23:54 -0700

    schedule renders

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -1,31 +1,32 @@
 import React from 'react'
 
 const Schedule = () => {
-  // return (
-  //
-  //   <table id="superSchedule">
-  //     <tr className = "text-center">
-  //       <th className = "col-lg-auto text-center">account.id</th>
-  //       <th className = "col-lg-auto text-center">email</th>
-  //       <th className = "col-lg-auto text-center">firstName1</th>
-  //       <th className = "col-lg-auto text-center">lastName1</th>
-  //       <th className = "col-lg-auto text-center">firstName2</th>
-  //       <th className = "col-lg-auto text-center">lastName2</th>
-  //       <th className = "col-lg-auto text-center">weddingDate</th>
-  //       <th className = "col-lg-auto text-center">account_id</th>
-  //       <th className = "col-lg-auto text-center">role</th>
-  //       <th className = "col-lg-auto text-center">template_id</th>
-  //       <th className = "col-lg-auto text-center">created_at</th>
-  //       <th className = "col-lg-auto text-center">updated_at</th>
-  //     </tr>
-  //
-  //     <% for (let i = 0; i < data.length; i++) { %>
-  //
-  //     <% } %>
-  //   </table>
-  //
-  //
-  // )
+  return (
+    <h1>Hi, Ken!</h1>
+
+    // <table id="superSchedule">
+    //   <tr className = "text-center">
+    //     <th className = "col-lg-auto text-center">account.id</th>
+    //     <th className = "col-lg-auto text-center">email</th>
+    //     <th className = "col-lg-auto text-center">firstName1</th>
+    //     <th className = "col-lg-auto text-center">lastName1</th>
+    //     <th className = "col-lg-auto text-center">firstName2</th>
+    //     <th className = "col-lg-auto text-center">lastName2</th>
+    //     <th className = "col-lg-auto text-center">weddingDate</th>
+    //     <th className = "col-lg-auto text-center">account_id</th>
+    //     <th className = "col-lg-auto text-center">role</th>
+    //     <th className = "col-lg-auto text-center">template_id</th>
+    //     <th className = "col-lg-auto text-center">created_at</th>
+    //     <th className = "col-lg-auto text-center">updated_at</th>
+    //   </tr>
+    //
+    //   <% for (let i = 0; i < data.length; i++) { %>
+    //
+    //   <% } %>
+    // </table>
+
+
+  )
 }
 
 
